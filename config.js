c640a8c4f77b96195fa970326f144861a327c54a
Hashan000-1 <pabasarahashan977@gmail.com>
2025-08-14 16:56:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Hashan000-1/PRINCESS-LUNA-WHATSAPP-BOT/refs/heads/main/images/fefe.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Itz ME 🥵👌 Luna Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
